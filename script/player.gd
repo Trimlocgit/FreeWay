fd4c31e02ddc50cd96672d83809f5897b1d6c2fb
FORTIN Victor <victor.fortin078@gmail.com>
2025-06-18 12:12:01 +0200

    ajout deplacement

--- script/player.gd
+++ script/player.gd
@@ -1,1 +1,20 @@
 extends Area2D
+
+
+@export var vitesse = 40 # Vitesse du joueur
+var tailleEcran # Taille de l'écran (limite de jeux)
+
+
+func _ready():
+	tailleEcran = get_viewport_rect().size
+	
+func _process(delta: float) -> void:
+	var velocite = Vector2.ZERO
+	
+	if (Input.is_action_pressed("droite")):
+		velocite.x += vitesse
+	if (Input.is_action_pressed("gauche")):
+		velocite.x -= vitesse
+		
+	position += velocite * delta
+	position = position.clamp(Vector2.ZERO, tailleEcran)
