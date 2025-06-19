2ec4c3b78a316e3352f875106f3f57029130fe61
FORTIN Victor <victor.fortin078@gmail.com>
2025-06-19 11:47:21 +0200

    Ajout collision joueur

--- script/player.gd
+++ script/player.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+signal hit
 
 @export var vitesse = 300 # Vitesse du joueur
 var tailleEcran # Taille de l'écran (limite de jeux)
@@ -19,3 +20,19 @@ func _process(delta: float) -> void:
 		
 	position += velocite * delta
 	position = position.clamp(Vector2.ZERO, tailleEcran)
+
+func _on_body_entered(body: Node2D) -> void:
+	vie -= 1
+	print(vie)
+	hit.emit()
+	if vie == 0:
+		hide()
+		$CollisionShape2D.set_deferred("disabled", true)
+	
+
+#Fonction appelé lors du lancement d'une nouvelle partie
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+	
